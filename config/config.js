27ec10d2c402be11e1fc40e9eb5081cdb19779d5
aemejuobi <govallen@gmail.com>
2019-03-11 21:10:17 -0500

    fixed config.js error

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var username = process.env.DB_USER;
 var password = process.env.DB_PASS;
-var database = process.env.DB_NAME
+var database = process.env.DB_NAME;
 var host = process.env.DB_HOST;
 
 module.exports = {
