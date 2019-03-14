19d32e8a333459f07e6f221ea730f37d91b10106
aemejuobi <govallen@gmail.com>
2019-03-13 21:16:04 -0500

    fixed database to sequelize and fixed route issues

--- models/schema.sql
+++ models/schema.sql
@@ -2,10 +2,3 @@ drop database if exists projectDB;
 CREATE DATABASE projectDB;
 
 use projectDB;
-create table user(
-    id int auto_increment,
-    name varchar(50) not null,
-    age int not null,
-    gender varchar(50) not null,
-    primary key (id)
-);
